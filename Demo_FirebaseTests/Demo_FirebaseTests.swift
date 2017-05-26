e4685dc4ee9792f0f23bbfe7520a9d6a3671abc1
Duy Bùi <duybui@Duys-Mac.local>
2017-05-26 23:31:55 +0700

    Initial Commit

---
+++ Demo_FirebaseTests/Demo_FirebaseTests.swift
@@ -0,0 +1,36 @@
+//
+//  Demo_FirebaseTests.swift
+//  Demo_FirebaseTests
+//
+//  Created by Duy Bùi on 5/26/17.
+//  Copyright © 2017 Duy Bùi. All rights reserved.
+//
+
+import XCTest
+@testable import Demo_Firebase
+
+class Demo_FirebaseTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
